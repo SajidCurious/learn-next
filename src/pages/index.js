cba242cd850c84b14171d9ab03ef307358cac1f6
Mohammed <sajidcurious9@gmail.com>
2023-04-28 16:38:06 +0530

    building nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        <ul className="flex justify-end px-12 gap-10">
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+      </nav>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p className="text-2xl">
