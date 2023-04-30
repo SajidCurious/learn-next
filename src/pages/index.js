e107285c36f99a0ceae6bb7083c5c946bd7ea948
Mohammed <sajidcurious9@gmail.com>
2023-05-01 02:07:54 +0530

    deleted navbar code and made a separate component for it

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,23 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="bg-black flex items-center justify-between">
-        <h1 className="text-white ml-12 font-bold text-2xl">Hunting Coder</h1>
-        <ul className="flex pr-12 gap-20 items-center py-4 text-white font-semibold">
-          <Link href="/">
-            <li className="hover:cursor-pointer">Home</li>
-          </Link>
-          <Link href="/blog">
-            <li className="hover:cursor-pointer">Blog</li>
-          </Link>
-          <Link href="/contact">
-            <li className="hover:cursor-pointer">Contact</li>
-          </Link>
-          <Link href="/about">
-            <li className="hover:cursor-pointer">About</li>
-          </Link>
-        </ul>
-      </nav>
+      <Navbar />
       <main>
         <div className="flex justify-center items-center mt-6 ml-6">
           <Image
