87e478e91bd4359c4d11c1242b0651078de4cf92
Mohammed <sajidcurious9@gmail.com>
2023-05-02 01:45:17 +0530

    developing the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const blog = () => {
-  return <div>My Blogs</div>;
+  return (
+    <div className="blogs mt-5 font-semibold flex flex-col items-center">
+      <h1 className="text-5xl">Popular Blogs</h1>
+      <div className="blogItems mt-10">
+        <div className="blogItem mb-4">
+          <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is used to design logic for the Web</p>
+        </div>
+        <div className="blogItem mb-4">
+          <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is used to design logic for the Web</p>
+        </div>
+        <div className="blogItem mb-4">
+          <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is used to design logic for the Web</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default blog;
