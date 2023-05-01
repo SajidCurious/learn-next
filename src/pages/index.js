0f1f0b783d4371f3e1f541a7dbe44d068a276713
Mohammed <sajidcurious9@gmail.com>
2023-05-02 01:34:46 +0530

    added navbar to the every page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +13,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
       <main>
         <div className="flex justify-center items-center mt-6 ml-6">
           <Image
