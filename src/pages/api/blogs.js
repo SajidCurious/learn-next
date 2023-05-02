56eab8ba775f56ad88b603670216740ea4a04c0a
Mohammed <sajidcurious9@gmail.com>
2023-05-03 01:56:46 +0530

    configuring apis in next.js

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -3,8 +3,11 @@
 import * as fs from "fs";
 
 export default function handler(req, res) {
-  fs.readFile("blogdata/learn-javascript.json", "utf-8", (err, data) => {
-    // console.log(data);
+  fs.readFile(`blogdata/${req.query.slug}.json`, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).json({ err: "No such blog found" });
+    }
+    console.log(req.query.slug);
     res.status(200).json(JSON.parse(data));
   });
 }
