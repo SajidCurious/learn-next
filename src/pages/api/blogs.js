bc29c14e0c16e334866e153d6ab83fc51c61aed6
Mohammed <sajidcurious9@gmail.com>
2023-05-03 02:19:39 +0530

    made an api to fetch all the blogs in the respective directory

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -3,11 +3,8 @@
 import * as fs from "fs";
 
 export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}.json`, "utf-8", (err, data) => {
-    if (err) {
-      res.status(500).json({ err: "No such blog found" });
-    }
-    console.log(req.query.slug);
-    res.status(200).json(JSON.parse(data));
+  fs.readdir("blogdata", (err, data) => {
+    console.log(data);
+    res.status(200).json(data);
   });
 }
