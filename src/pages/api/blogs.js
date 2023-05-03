d33920ed54a71f504d78fa62b07b7b76b38c6600
Mohammed <sajidcurious9@gmail.com>
2023-05-04 01:12:37 +0530

    successfully read data from json file using next file directory and displayed it on the blogs.js in json format

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -4,7 +4,16 @@ import * as fs from "fs";
 
 export default async function handler(req, res) {
   let data = await fs.promises.readdir("blogdata");
-  res.status(200).json(data);
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item);
+    myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    console.log(myfile);
+    allBlogs.push(JSON.parse(myfile));
+  }
+  res.status(200).json(allBlogs);
   // fs.readdir("blogdata", (err, data) => {
   //   console.log(data);
   //   res.status(200).json(data);
