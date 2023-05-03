1720ff1529bb2bc1df9b5ffaf5a03e1e9e529b15
Mohammed <sajidcurious9@gmail.com>
2023-05-04 00:39:56 +0530

    edited blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,21 +6,21 @@ const blog = () => {
     <div className="blogs mt-5 font-semibold flex flex-col items-center">
       <h1 className="text-5xl">Popular Blogs</h1>
       <div className="blogItems mt-10">
-        <div className="blogItem mb-4">
+        <div className="blogItem mb-6">
           <Link href={"/blogpost/learn-javascript"}>
-            <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+            <h1 className="text-2xl">How to learn JavaScript in 2022?</h1>
           </Link>
           <p>JavaScript is used to design logic for the Web</p>
         </div>
-        <div className="blogItem mb-4">
+        <div className="blogItem mb-6">
           <Link href={"/blogpost/learn-javascript"}>
-            <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+            <h1 className="text-2xl">How to learn JavaScript in 2022?</h1>
           </Link>
           <p>JavaScript is used to design logic for the Web</p>
         </div>
-        <div className="blogItem mb-4">
+        <div className="blogItem mb-6">
           <Link href={"/blogpost/learn-javascript"}>
-            <h1 className="text-xl">How to learn JavaScript in 2022?</h1>
+            <h1 className="text-2xl">How to learn JavaScript in 2022?</h1>
           </Link>
           <p>JavaScript is used to design logic for the Web</p>
         </div>
