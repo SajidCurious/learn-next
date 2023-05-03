e04baff671b20a1dcba022c370aca2890fc8764b
Mohammed <sajidcurious9@gmail.com>
2023-05-04 00:57:31 +0530

    trying to use the json file data into the blogs page

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -2,9 +2,11 @@
 
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  res.status(200).json(data);
+  // fs.readdir("blogdata", (err, data) => {
+  //   console.log(data);
+  //   res.status(200).json(data);
+  // });
 }
