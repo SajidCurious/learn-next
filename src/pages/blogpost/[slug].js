d09252d6222af6c9e8f66a18b5939fa8c86943dd
Mohammed <sajidcurious9@gmail.com>
2023-05-04 15:28:29 +0530

    edited the blog

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const slug = () => {
+  const [blog, setBlog] = useState();
   const router = useRouter();
-  const { slug } = router.query;
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { slug } = router.query;
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlog(parsed);
+      });
+  }, [router.isReady]);
+
   return (
     <div className="flex flex-col items-center">
-      <h1 className="font-bold text-2xl mt-6">Title of the page {slug}</h1>
-      <p className="max-w-6xl mt-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum explicabo
-        possimus deleniti ab minus dolorem quisquam eos nemo ipsa maiores qui
-        eum natus quam quas tenetur, laboriosam assumenda. Ducimus soluta
-        incidunt debitis delectus, cupiditate exercitationem voluptatibus
-        voluptates quod cumque consequatur accusantium dolor consequuntur dolore
-        hic mollitia fugit provident magnam? Consectetur non quasi quo iusto
-        fuga, modi dolorem architecto nam, est ex voluptates beatae ipsum
-        doloremque eos excepturi neque cum delectus repellat ad magnam molestias
-        dolore quidem. Quasi corrupti modi aperiam cumque quaerat eligendi
-        tempore possimus! Est incidunt laboriosam deleniti nostrum, doloremque
-        quas error eveniet excepturi, magnam minima tempore sunt placeat.
-      </p>
+      <h1 className="font-bold text-2xl mt-6">{blog && blog.title}</h1>
+      <p className="max-w-6xl mt-5">{blog && blog.content}</p>
     </div>
   );
 };
