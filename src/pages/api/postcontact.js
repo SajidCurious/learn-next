22b23da2f76e077eb31022369e96d1d8423c941a
Mohammed <sajidcurious9@gmail.com>
2023-05-07 21:43:57 +0530

    testing the sample

--- src/pages/api/postcontact.js
+++ src/pages/api/postcontact.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
+    console.log(req.body);
     res.status(200).json(["kkkkkkkkkk"]);
   } else {
     // Handle any other HTTP method
