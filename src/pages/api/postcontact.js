cbda5d226cfba986f7ff58ea0cf838c3a188d1bc
Mohammed <sajidcurious9@gmail.com>
2023-05-07 19:47:33 +0530

    checking the post request

--- src/pages/api/postcontact.js
+++ src/pages/api/postcontact.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
+    res.status(200).json(["kkkkkkkkkk"]);
   } else {
     // Handle any other HTTP method
+    res.status(200).json(["jjjjjjjj"]);
   }
 }
