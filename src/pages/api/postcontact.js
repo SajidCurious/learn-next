b65d1114a019f8957a4d43ddb67210e75f0aa69a
Mohammed <sajidcurious9@gmail.com>
2023-05-08 01:31:22 +0530

    figuring out why my api is not working

--- src/pages/api/postcontact.js
+++ src/pages/api/postcontact.js
@@ -2,9 +2,10 @@ import * as fs from "fs";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
+    console.log(req.body);
     let data = await fs.promises.readdir("contactdata");
     fs.promises.writeFile(
-      `contactdata/${data.length + 1}`,
+      `contactdata/${data.length + 1}.json`,
       JSON.stringify(req.body)
     );
     res.status(200).json(req);
