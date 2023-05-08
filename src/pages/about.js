c283e6fc5fe5bd9722dc0a2d1209ff0c8bab6e43
Mohammed <sajidcurious9@gmail.com>
2023-05-09 00:39:28 +0530

    About design is completed basically

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const about = () => {
   return (
-    <div className="m-auto max-w-[45vw] flex flex-col items-center mt-6">
-      <h1 className="text-3xl font-semibold">About Hunting Coder</h1>
+    <div className="m-auto max-w-[45vw] mt-6">
       <h2 className="text-2xl mt-5 font-semibold">Introduction</h2>
-      <p className="mt-5 font-medium">
+      <p className="mt-1 font-medium">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea vel,
         deserunt deleniti sequi quia sit asperiores quae consequatur, non eos
         culpa rerum tempora dolores fugiat harum eaque eum cumque quas corrupti
@@ -22,34 +21,27 @@ const about = () => {
         praesentium expedita nam error a velit omnis totam, ad eius? Ipsa saepe
         dignissimos quas facilis minus vitae necessitatibus dolor optio quod
         vero velit voluptatibus nemo qui ex fuga sequi aperiam nisi quam iste
-        consequuntur, eum quisquam nihil, iure ea! Ullam dicta modi mollitia eum
-        ipsam omnis sint. Ullam quidem, explicabo tenetur id, doloremque
-        repudiandae facilis neque illo eos, tempore quae laborum quaerat
-        laudantium consequuntur! Illo architecto rerum eius unde molestiae
-        praesentium obcaecati doloremque eligendi dolores eum, eos corrupti
-        deserunt id animi cumque ducimus est possimus libero? In reprehenderit
-        temporibus mollitia qui nisi! Commodi explicabo vitae deserunt qui iste
-        perspiciatis impedit earum quibusdam quisquam nihil culpa ratione,
-        aspernatur assumenda debitis quasi aut nulla facere ea accusamus
-        necessitatibus velit, numquam similique! A perferendis libero cumque
-        voluptatibus eligendi quibusdam enim accusantium, quaerat tempore rerum,
-        qui dolore quo itaque sequi distinctio ex in facere. Consequatur sit
-        amet ducimus ipsum nisi dicta eos exercitationem repudiandae, cumque
-        corporis aspernatur doloremque facilis dolore sed architecto aut.
-        Temporibus non accusantium quo eos! Deleniti modi veniam molestias nihil
-        praesentium enim laboriosam quia a, harum ullam fuga aspernatur error
-        soluta, tenetur atque minus fugit iusto, nulla quis! Odit architecto
-        necessitatibus dolorem labore? Ducimus ab reiciendis necessitatibus,
-        rerum magni est dolorum qui velit consectetur fugiat perferendis maxime
-        eveniet harum a error. Mollitia, dicta. Deserunt tempore perferendis
-        deleniti dignissimos libero voluptatem ad hic provident quam doloribus
-        doloremque quia error minus totam illo adipisci, sit minima ea obcaecati
-        animi, officia ut cupiditate unde fugiat. Quasi dolorum consectetur
-        eaque deserunt error pariatur ut maiores earum labore, debitis voluptate
-        alias ducimus amet vero quibusdam nesciunt illum! Sequi nulla
-        consequatur minus quam pariatur quasi. Ipsum, magni officia totam cumque
-        quas adipisci, earum animi laudantium sapiente laboriosam architecto?
-        Explicabo, suscipit.
+        consequuntur consequatur minus quam pariatur quasi. Ipsum, magni officia
+        totam cumque quas adipisci, earum animi laudantium sapiente laboriosam
+        architecto? Explicabo, suscipit.
+      </p>
+      <h2 className="text-xl mt-5 font-semibold">Services Offered</h2>
+      <p className="mt-1 font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        consequatur? Sapiente officiis sit, consequuntur quaerat id, obcaecati
+        aspernatur laudantium facere culpa deleniti esse aliquam impedit optio
+        cumque maiores repellendus? Repellendus, error voluptatem. Quisquam
+        quidem enim minima dolor culpa unde ipsum alias possimus, laboriosam
+        quos! Vero quisquam quod ex at laudantium!
+      </p>
+      <h2 className="text-xl mt-5 font-semibold">Contact Us</h2>
+      <p className="mt-1 font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        consequatur? Sapiente officiis sit, consequuntur quaerat id, obcaecati
+        aspernatur laudantium facere culpa deleniti esse aliquam impedit optio
+        cumque maiores repellendus? Repellendus, error voluptatem. Quisquam
+        quidem enim minima dolor culpa unde ipsum alias possimus, laboriosam
+        quos! Vero quisquam quod ex at laudantium!
       </p>
     </div>
   );
