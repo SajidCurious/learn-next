affa124db2c0182a3eb43a7c6e48d8102a2c86a0
Mohammed <sajidcurious9@gmail.com>
2023-05-09 00:05:51 +0530

    adding elements in the about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,58 @@
 import React from "react";
 
 const about = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea vel,
+        deserunt deleniti sequi quia sit asperiores quae consequatur, non eos
+        culpa rerum tempora dolores fugiat harum eaque eum cumque quas corrupti
+        nihil. Itaque quasi nostrum voluptates quos dolor necessitatibus
+        asperiores ipsam aut nisi placeat voluptate doloribus culpa odit quas
+        natus, quia, beatae illum suscipit optio impedit! Quibusdam blanditiis
+        aspernatur optio libero asperiores itaque eaque numquam accusantium
+        excepturi obcaecati harum minus quas quo facere ab incidunt non,
+        perferendis animi quaerat dolores nostrum! Ullam, non aliquam! Molestias
+        eligendi consequuntur expedita adipisci tempore natus in, doloribus
+        voluptas deserunt sint numquam reiciendis earum quos provident ipsum,
+        officia fugit aliquam voluptatibus mollitia corrupti dolor ut ipsam
+        perspiciatis culpa. Temporibus voluptatem, accusantium doloribus
+        praesentium expedita nam error a velit omnis totam, ad eius? Ipsa saepe
+        dignissimos quas facilis minus vitae necessitatibus dolor optio quod
+        vero velit voluptatibus nemo qui ex fuga sequi aperiam nisi quam iste
+        consequuntur, eum quisquam nihil, iure ea! Ullam dicta modi mollitia eum
+        ipsam omnis sint. Ullam quidem, explicabo tenetur id, doloremque
+        repudiandae facilis neque illo eos, tempore quae laborum quaerat
+        laudantium consequuntur! Illo architecto rerum eius unde molestiae
+        praesentium obcaecati doloremque eligendi dolores eum, eos corrupti
+        deserunt id animi cumque ducimus est possimus libero? In reprehenderit
+        temporibus mollitia qui nisi! Commodi explicabo vitae deserunt qui iste
+        perspiciatis impedit earum quibusdam quisquam nihil culpa ratione,
+        aspernatur assumenda debitis quasi aut nulla facere ea accusamus
+        necessitatibus velit, numquam similique! A perferendis libero cumque
+        voluptatibus eligendi quibusdam enim accusantium, quaerat tempore rerum,
+        qui dolore quo itaque sequi distinctio ex in facere. Consequatur sit
+        amet ducimus ipsum nisi dicta eos exercitationem repudiandae, cumque
+        corporis aspernatur doloremque facilis dolore sed architecto aut.
+        Temporibus non accusantium quo eos! Deleniti modi veniam molestias nihil
+        praesentium enim laboriosam quia a, harum ullam fuga aspernatur error
+        soluta, tenetur atque minus fugit iusto, nulla quis! Odit architecto
+        necessitatibus dolorem labore? Ducimus ab reiciendis necessitatibus,
+        rerum magni est dolorum qui velit consectetur fugiat perferendis maxime
+        eveniet harum a error. Mollitia, dicta. Deserunt tempore perferendis
+        deleniti dignissimos libero voluptatem ad hic provident quam doloribus
+        doloremque quia error minus totam illo adipisci, sit minima ea obcaecati
+        animi, officia ut cupiditate unde fugiat. Quasi dolorum consectetur
+        eaque deserunt error pariatur ut maiores earum labore, debitis voluptate
+        alias ducimus amet vero quibusdam nesciunt illum! Sequi nulla
+        consequatur minus quam pariatur quasi. Ipsum, magni officia totam cumque
+        quas adipisci, earum animi laudantium sapiente laboriosam architecto?
+        Explicabo, suscipit.
+      </p>
+    </div>
+  );
 };
 
 export default about;
